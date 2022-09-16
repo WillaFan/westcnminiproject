66a4be78896f447694bdc4260bf10b25d355a7ce
WillaFan <52628925+WillaFan@users.noreply.github.com>
2022-09-16 15:15:41 +0800

    Add files via upload

--- src/main/java/com/fdmgroup/iwish/SoloProjectApplication.java
+++ src/main/java/com/fdmgroup/iwish/SoloProjectApplication.java
@@ -2,8 +2,10 @@ package com.fdmgroup.iwish;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SoloProjectApplication {
 
 	public static void main(String[] args) {
